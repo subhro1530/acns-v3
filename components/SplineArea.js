64b73781cba0d5db7a842ade43566bc959ef117c
subhro1530 <shaswata.ssaha@gmail.com>
2023-10-18 01:13:48 +0530

    made the spline 3D design

--- components/SplineArea.js
+++ components/SplineArea.js
@@ -1,41 +1,49 @@
-// components/GetStarted.js
-
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/SplineArea.module.css";
 import Spline from "@splinetool/react-spline";
-import { Application } from "@splinetool/runtime";
-import { useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 
+const WelcomeText = ({ text }) => {
+  const [displayText, setDisplayText] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Display the text one character at a time
+      setDisplayText((prevText) => {
+        if (prevText.length < text.length) {
+          return text.slice(0, prevText.length + 1);
+        }
+        clearInterval(interval); // Stop the interval when text is fully displayed
+        return prevText;
+      });
+    }, 100); // Adjust the interval duration as needed
+
+    return () => clearInterval(interval); // Cleanup on component unmount
+  }, [text]);
+
+  return <h1>{displayText}</h1>;
+};
+
 const SplineArea = () => {
-  const [myObj, setMyObj] = useState(null);
-  function onLoad(spline) {
-    const obj = spline.findObjectById("5bf7fb81-6117-422d-97d1-e247833be1ab");
-    // or
-    // const obj = spline.findObjectByName('my object');
-    spline.setZoom(1.0);
-    setMyObj(obj);
-  }
-  function moveObj() {
-    console.log(myObj); // Spline Object => { name: 'my object', id: '8E8C2DDD-18B6-4C54-861D-7ED2519DE20E', position: {}, ... }
-    // move the object in 3D space
-    myObj.position.x += 10;
-  }
   return (
-    <>
-      <div className={styles.body}>
-        <Spline
-          className={styles.spline}
-          scene="https://prod.spline.design/VQvhnqAjjZ5ITnKM/scene.splinecode"
-          onLoad={onLoad}
+    <div className={styles.body}>
+      {/* Your Spline component */}
+      <Spline
+        className={styles.spline}
+        scene="https://prod.spline.design/VQvhnqAjjZ5ITnKM/scene.splinecode"
+      />
+
+      {/* Welcome text */}
+      <div className={styles.scroll}>
+        <Image
+          src="/Profile_new.jpg"
+          className={styles.profile}
+          width={100}
+          height={100}
         />
-        <div className={styles.scroll}>
-          <Image src="/Profile_new.jpg" className={styles.profile} width={70} height={70}></Image>
-          <h1>Hey there! Welcome to ACNS...</h1>
-        </div>
+        <WelcomeText text="ACNS: Navigating Cosmic Discoveries Together..." />
       </div>
-    </>
+    </div>
   );
 };
 
